51883e315401920b55e11f64f693ee2e6ad4f3bf
Jason Butwell <jasonbutwell@gmail.com>
2016-10-13 20:43:01 +0100

    Added a Dialog that will appear and ask the user to set the language if it is not set. Once set the preference is saved using sharedPreferences. Upon running the app again, the app picks up the value and thus does not ask the user again and the Dialog does not appear. The user now has the option to change the language using the options menu located in the top right of the ActionBar. Making a selection here will change the language value and save the preferrence, so again the app will retrieve that preference when it is run again.

--- app/src/main/java/com/jasonbutwell/sharedpreferenceslanguagedemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/sharedpreferenceslanguagedemo/MainActivity.java
@@ -1,30 +1,132 @@
 package com.jasonbutwell.sharedpreferenceslanguagedemo;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferences;
+    private String [] languages = { "English", "Spanish" };
+    private String languageToUse = "";
+    private int languageID = -1;
+
+    private TextView languageTextView;
+
+    private void updateLanguageView () {
+        TextView languageTextView = (TextView) findViewById( R.id.languageTextView );
+        languageTextView.setText( "Language is set to : " + languages[languageID] ); // show what language is set to.
+    }
+    // sets the language id and then saves the language based on ID
+    private void setLanguageAndSave( int id ) {
+        languageID = id;
+        saveLanguagePrefence( languages[ languageID ] );
+        updateLanguageView ();
+    }
+
+    // saves the preference
+    private void saveLanguagePrefence( String preference ) {
+        sharedPreferences.edit().putString( "language", preference ).apply();
+    }
+
+    // attempts to get the preference
+    private String getLanguagePreference() {
+        return sharedPreferences.getString( "language", "" );
+    }
+
+    // get array index value of the string from the array
+    private int getLanguageID() {
+        int id = -1;
+
+        for (int i = 0; i < languages.length; i ++) {
+            if ( languages[i].equals(languageToUse) ) {
+                id = i;
+                break;  // exit the loop if found
+            }
+        }
+
+        return id;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = this.getSharedPreferences( "com.jasonbutwell.sharedpreferencesdemo", Context.MODE_PRIVATE );
+
+        // if no preferences were saved previously then do the alert and ask the user
+
+        // get the language string
+        languageToUse = getLanguagePreference();
+//
+//        // check to see if it was set and request a language if not
+
+        if ( languageToUse.isEmpty() )
+             doAlert();                                              // calls the alert
+
+        Log.i("language", languageToUse);
+
+        languageID = getLanguageID();
+
+        if (languageID != -1 )
+            updateLanguageView();
+
+        // Clear the value for testing purposes
+        //sharedPreferences.edit().remove("language").commit();
+    }
+
+    private void doAlert() {
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+        // stop user cancelling the dialog
+        alertDialog.setCanceledOnTouchOutside( false );
+        alertDialog.setCancelable( false );
+
+        // set up the other fields
+        alertDialog.setTitle("User Prompt");
+        alertDialog.setMessage("Which language would you prefer to use?");
+
+        // button and listener
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, languages[0],
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        setLanguageAndSave( 0 );
+                        dialog.dismiss();
+                    }
+                });
+
+        // button and listener
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, languages[1],
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        setLanguageAndSave( 1 );
+                        dialog.dismiss();
+                    }
+                });
+
+        // show the dialog
+        alertDialog.show();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu( Menu menu ) {
 
         // Inflate the menu; this adds items to the action bar if it is present.
 
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate( R.menu.menu_main, menu );
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected( MenuItem item ) {
 
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -38,6 +140,8 @@ public class MainActivity extends AppCompatActivity {
 
         if (id == R.id.action_settings1) {
             Log.i("select","Item 1 selected");
+            setLanguageAndSave( 0 );
+            updateLanguageView();
             return true;
         }
 
@@ -45,6 +149,8 @@ public class MainActivity extends AppCompatActivity {
 
         if (id == R.id.action_settings2) {
             Log.i("select","Item 2 selected");
+            setLanguageAndSave( 1 );
+            updateLanguageView();
             return true;
         }
 
